2ed30fce54ca0e9963c115ec787baaff147fd3f1
iMex-1 <moukeddar236med@gmail.com>
2025-10-28 11:26:38 +0100

    added cart, navbar and routing

---
+++ components/navbar.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  const { state } = useContext(CartContext);
+
+  return (
+    <nav className="navbar navbar-dark bg-dark p-3 d-flex justify-content-between">
+      <Link to="/" className="text-white text-decoration-none">
+        <h3>🛒 E-Shop</h3>
+      </Link>
+
+      <Link to="/cart" className="text-white text-decoration-none">
+        Panier : {state.cart.length} produits
+      </Link>
+    </nav>
+  );
+}
