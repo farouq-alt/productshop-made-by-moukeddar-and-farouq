bad6c8dfbdffbd41c8c4b740d3d42ecad076f501
iMex-1 <moukeddar236med@gmail.com>
2025-10-28 11:18:22 +0100

    added app

--- App.jsx
+++ App.jsx
@@ -1,6 +1,13 @@
-import React from "react";
-
-export default function App(){
-
-
+import ListProduct from "./Components/G-Prods/productshop/components/productlist";
+import { CartProvider } from "./Components/G-Prods/productshop/context/CartContext";
+function App() {
+  return (
+    <div>
+      <CartProvider>
+        <ListProduct />
+      </CartProvider>
+    </div>
+  );
 }
+
+export default App;
